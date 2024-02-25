d4b47437be4aaec4a23950c8a1e3938da1245335
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2024-02-25 15:22:21 +0900

    colored error message

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
+use colored::Colorize;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    "hoge
+    "
+    .red();
     let target_md_paths = if args.len() > 1 {
         &args[1..]
     } else {
         panic!(
-            r#"
+            "{}",
+            "
 ---------------------------
 target file is NOT given.
-please use obsidian-md-normalize "path_to_md" "path_to_md" ...
+please use obsidian-md-normalize \"path_to_md\" \"path_to_md\" ...
 ---------------------------
-            "#
+            "
+            .red()
         );
     };
     println!("{:?}", target_md_paths);
@@ -21,10 +27,15 @@ please use obsidian-md-normalize "path_to_md" "path_to_md" ...
             Ok(file) => file,
             Err(ref err) => {
                 if err.kind() == std::io::ErrorKind::NotFound {
-                    panic!("file: [{target_md_path}] is not found.\n{:?}", err);
+                    panic!(
+                        "{}\n{:?}",
+                        "file: [{target_md_path}] is not found.".red(),
+                        err
+                    );
                 } else {
                     panic!(
-                        "UNKNOWN PROBLEM was happend on opening the file: [{target_md_path}]\n{:?}",
+                        "{}\n{:?}",
+                        "UNKNOWN PROBLEM was happend on opening the file: [{target_md_path}]".red(),
                         err
                     );
                 }
@@ -32,7 +43,11 @@ please use obsidian-md-normalize "path_to_md" "path_to_md" ...
         };
         let mut contents = String::new();
         if let Err(err) = target_md.read_to_string(&mut contents) {
-            panic!("file: [{target_md_path}] COULD NOT be read.\n{:?}", err);
+            panic!(
+                "{}\n{:?}",
+                "file: [{target_md_path}] COULD NOT be read.".red(),
+                err
+            );
         }
         println!("{}", contents);
     }
